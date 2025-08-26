3b604996216fe767bad2208ab1e15ed319b0ae9a
aniketbumbc <aniket.umbc@gmail.com>
2025-08-26 15:44:09 +0530

    initial setup llm

---
+++ llm invoke/app.js
@@ -0,0 +1,33 @@
+import Groq from 'groq-sdk';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load .env variables
+
+const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+
+const main = async () => {
+  const response = await groq.chat.completions.create({
+    model: 'llama-3.3-70b-versatile',
+    messages: [
+      {
+        role: 'system',
+        content: ` You are Bunny, You are a sentiment analysis assistant.
+           Given any product review, respond with only one word:
+         "positive", "negative", or "neutral" — depending on the overall sentiment of the review.
+         Do not include any explanations, punctuation, or extra words.`,
+      },
+      {
+        role: 'user',
+        content: `Review: The headphone sound quality is okkiesh class.
+                Sentiment:`,
+      },
+    ],
+  });
+
+  console.log(response.choices[0].message.content);
+  //   console.log(response);
+};
+
+main();
+
+//https://console.groq.com/docs/overview
