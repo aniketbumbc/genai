723e2f5ffe2e4de9f01d0b33d81dd4d76d661d8b
aniketbumbc <aniket.umbc@gmail.com>
2025-08-27 06:36:02 +0530

    parameters llm

--- llm invoke/app.js
+++ llm invoke/app.js
@@ -8,6 +8,11 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const main = async () => {
   const response = await groq.chat.completions.create({
     model: 'llama-3.3-70b-versatile',
+    temperature: 1,
+    // top_p: 0.2,
+    // stop: 'ral',
+    // max_completion_tokens: 1000,
+    // max_tokens:1000,
     messages: [
       {
         role: 'system',
