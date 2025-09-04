77c7a48046f3f26cacf8d6cc219e1c07e73df4cd
aniketbumbc <aniket.umbc@gmail.com>
2025-09-04 12:35:52 +0530

    fetching data from vector store

--- rag system/comapy-bot/ragServer.js
+++ rag system/comapy-bot/ragServer.js
@@ -1,4 +1,10 @@
-import { indexDocument } from './docIndex.js';
+import { indexDocument, vectorStore } from './docIndex.js';
 const docPath = './cg-internal-docs.pdf';
 
-indexDocument(docPath);
+//indexDocument(docPath);
+
+const similaritySearchResults = await vectorStore.similaritySearch('leave');
+
+for (const doc of similaritySearchResults) {
+  console.log(`* ${doc.pageContent} [${JSON.stringify(doc.metadata, null)}]`);
+}
