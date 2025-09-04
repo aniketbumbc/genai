0f2c80bee67b4094bd4f20969fcbf987721e5e7f
aniketbumbc <aniket.umbc@gmail.com>
2025-09-04 12:29:33 +0530

    documents store in vector db as chunks with embeddings

--- rag system/comapy-bot/docIndex.js
+++ rag system/comapy-bot/docIndex.js
@@ -1,20 +1,43 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
 import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { OpenAIEmbeddings } from '@langchain/openai';
+import { PineconeStore } from '@langchain/pinecone';
+import { Pinecone as PineconeClient } from '@pinecone-database/pinecone';
+import dotenv from 'dotenv';
+dotenv.config(); // Load .env variables
+
+const embeddings = new OpenAIEmbeddings({
+  model: 'text-embedding-3-small',
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const pinecone = new PineconeClient({ apiKey: process.env.PINECONE_API_KEY });
+
+const pineconeIndex = pinecone.Index(process.env.PINECONE_INDEX);
+
+export const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+  pineconeIndex,
+  maxConcurrency: 5,
+});
 
 export const indexDocument = async (path) => {
+  // Load the pdf
   const loader = new PDFLoader(path, { splitPages: false });
   const doc = await loader.load();
 
+  // chunk the data from pdf
   const textSplitter = new RecursiveCharacterTextSplitter({
     chunkSize: 500,
     chunkOverlap: 100,
   });
 
   const texts = await textSplitter.splitText(doc[0].pageContent);
-  console.log(texts);
-  texts.forEach((chunk, i) => {
-    console.log(`Chunk ${i + 1}: ${chunk.length} characters`);
-    console.log(chunk);
-    console.log('='.repeat(80)); // visual separator
+
+  const documents = texts.map((chunk, i) => {
+    return {
+      pageContent: chunk,
+      metadata: doc[0].metadata,
+    };
   });
+
+  await vectorStore.addDocuments(documents);
 };
