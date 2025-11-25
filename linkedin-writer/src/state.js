8ad0d77d4e85993153a82bee8bce0e56317b8516
aniketbumbc <aniket.umbc@gmail.com>
2025-11-25 13:19:30 +0530

    comment of state code

--- linkedin-writer/src/state.js
+++ linkedin-writer/src/state.js
@@ -1,5 +1,8 @@
 import { Annotation, MessagesAnnotation } from '@langchain/langgraph';
 
+/**
+ *  Initial state of application
+ */
 export const LinkedinState = Annotation.Root({
   ...MessagesAnnotation.spec,
 });
