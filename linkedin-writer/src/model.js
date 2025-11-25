ebc613f5a32d88dcfc08d89bc2d16dc5a1716782
aniketbumbc <aniket.umbc@gmail.com>
2025-11-25 13:23:59 +0530

    more comment on model

--- linkedin-writer/src/model.js
+++ linkedin-writer/src/model.js
@@ -1,7 +1,10 @@
 import dotenv from 'dotenv';
 import { ChatOpenAI } from '@langchain/openai';
 
-/** to get env variable */
+/** to get env variable
+ * Open ai env variable
+ *
+ */
 dotenv.config();
 
 export const model = new ChatOpenAI({
